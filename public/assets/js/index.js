9f2632dfaf7cc61abc094c8f51cb02924354fdcf
Nick Johnson <nicklynnjohnson@gmail.com>
2021-10-22 19:06:47 -0700

    Attempts at adding id to db not working. Saving work for now though.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,6 +3,7 @@ const $noteAside = $(".note-aside");
 const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
+const $noteList = $(".list-container .list-group");
 
 // All db functions
 
@@ -12,16 +13,24 @@ var getNotes = function() {
         url: "/api/notes",
         method: "GET"
     });
-}
+};
 
 //// Save a new note to the db
-const saveNote = function(note) {
+var saveNote = function(note) {
     return $.ajax({
       url: "/api/notes",
       data: note,
       method: "POST",
     });
-  };
+};
+
+//// Delete a note from the db
+var deleteNote = function(id) {
+    return $.ajax({
+      url: "api/notes/" + id,
+      method: "DELETE"
+    });
+};
   
 
 // Render note list from db
@@ -47,33 +56,44 @@ var renderDB = function(allNotes) {
 
     // Put the now changed notes where the emptied noteAside is
     $noteAside.append(changedAllNotes);
-}
+};
 
-// Save note functions
-
-//// Get the note data from inputs
-
+// Save note function
 const handleNoteSave = function() {
     const newNote = {
         title: $noteTitle.val(),
         text: $noteText.val()
     }
 
+    // Once note is gleaned from the field, send it to the db
     saveNote(newNote);
+    // Automatically display the new db
     getAndRenderNotes();
-}
+};
 
+// Delete note function
+const handleNoteDelete = function(event) {
+    // prevents the click listener for the list from being called when the button inside of it is clicked
+    event.stopPropagation();
+
+    const note = $(this).parent(".list-group-item").data();
+
+    deleteNote(note.id);
+    getAndRenderNotes(); 
+};
 
 // (See Execute call at bottom) Functions to get already-stored db entries and display them to the notes aside
 var getAndRenderNotes = function() {
     return getNotes().then(function(data) {
         renderDB(data);
     })
-}
+};
 
 // All click handlers
 $saveNoteBtn.on("click", handleNoteSave);
 
+//// "delete-note" doesn't exist from the html. Instead, it gets created via js when an entry is saved to/or already exists in, the db
+$noteList.on("click", ".delete-note", handleNoteDelete);
 
 // (Execute) Functions to get already-stored db entries and display them to the notes aside
 getAndRenderNotes();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,11 +15,43 @@ module.exports = function(app) {
 
   // POST Requests
 
+  //// Note: be able to eventually update the db
+
+  function writeNewNoteDB(newNotes) {
+    freshNotes = JSON.stringify(newNotes);
+    console.log(freshNotes);
+    fs.writeFile("../db/db", freshNotes, (err) => {
+      if (err) {
+        return (err);
+      } else {
+        console.log("Note successfully saved.")
+      }
+    })
+  }
+
   app.post("/api/notes", function(req, res) {
 
+    console.log(dbNotes.length);
+
+    // We'll need each new note to have an id so it's easier to delete the note later
+    // Lets add some vars so it's easier to read
+    var newID = req.body.id;
+    // Enable the ids to increase based on the amount of entries expanding in the db
+    if (dbNotes.length = 0) {
+      newID = 1;
+    } else {
+      newID = dbNotes.length + 1;
+    }
+
+    // Add "newID" to the newNote we want to push
+    var addedNewNote = req.body;
+    addedNewNote.id = newID;
+
     // Take req from the js file and put it into the db we called so it gets displayed on the side view
-    dbNotes.push(req.body);
+    dbNotes.push(addedNewNote);
     console.log(dbNotes);
+
+    writeNewNoteDB(dbNotes);
 
   })
 
