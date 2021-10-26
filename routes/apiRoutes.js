c66c5361207863c91c2b33d7f53fce8a5b427be7
Nick Johnson <nicklynnjohnson@gmail.com>
2021-10-26 08:09:47 -0700

    Got post + writefile to work.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,11 +15,11 @@ module.exports = function(app) {
 
   // POST Requests
 
-  //// Note: be able to eventually update the db
+  //// Note: have a write funtion handy to update the db file with new post requests
 
   function writeNewNoteDB(newNotes) {
     freshNotes = JSON.stringify(newNotes);
-    console.log(freshNotes);
+    
     fs.writeFile("../db/db", freshNotes, (err) => {
       if (err) {
         return (err);
@@ -31,13 +31,11 @@ module.exports = function(app) {
 
   app.post("/api/notes", function(req, res) {
 
-    console.log(dbNotes.length);
-
     // We'll need each new note to have an id so it's easier to delete the note later
     // Lets add some vars so it's easier to read
     var newID = req.body.id;
     // Enable the ids to increase based on the amount of entries expanding in the db
-    if (dbNotes.length = 0) {
+    if (dbNotes.length == 0) {
       newID = 1;
     } else {
       newID = dbNotes.length + 1;
@@ -49,10 +47,9 @@ module.exports = function(app) {
 
     // Take req from the js file and put it into the db we called so it gets displayed on the side view
     dbNotes.push(addedNewNote);
-    console.log(dbNotes);
 
+    // Now write the db file with the new array
     writeNewNoteDB(dbNotes);
-
   })
 
   
